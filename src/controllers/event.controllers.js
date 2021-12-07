328a75d651d6ce4cbc97e1bc24352366c8e5d0d5
[Arranzazu] <[Arranzazu@outlook.es]>
2021-12-07 08:11:44 +0100

    act071221

--- src/controllers/event.controllers.js
+++ src/controllers/event.controllers.js
@@ -8,69 +8,64 @@ const models = require("../models");
 // moment.locale("es");
 
 const create = async (req, res) => {
-	try {
-		const { name, date, cars, activo } = req.body;
-		if (!name || !date || !cars ) {
-			return res.status(409).json({ error: 'Todos los campos son requeridos' });
-		}
-        var moment = require('moment'); // require
-        moment(date).format('l');
-		const event = models.event({ name, date, cars, activo });
-		await event.save();
+  try {
+    console.log({ body: req.body });
+    const { name, date, cars, activo } = req.body;
+    if (!name || !date || !cars) {
+      return res.status(409).json({ error: "Todos los campos son requeridos" });
+    }
 
-		return res.status(201).json({ event });
-	} catch (_) {
-		return res.status(409).json({ error: 'Hubo un error al crear el evento (Server)' });
-	}
+    const event = models.event({ name, date, cars, activo });
+    await event.save();
+
+    return res.status(201).json({ event });
+  } catch (_) {
+    return res
+      .status(409)
+      .json({ error: "Hubo un error al crear el evento (Server)" });
+  }
 };
-
-
 
 const all = async (req, res) => {
-    try {
-        const events = await models.event.find();
+  try {
+    const events = await models.event.find().sort({ date: "asc" });
 
-        return res.json({ events })
-
-
-    } catch (err) {
-        return res.json({ error: 'no se pudo crear el listado' });
-    }
+    return res.json({ events });
+  } catch (err) {
+    return res.json({ error: "no se pudo crear el listado" });
+  }
 };
-
 
 const get = async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const event = await models.event.findById(id)
-		return res.json({ event });
-	} catch (_) {
-		return res.status(409).json({ error: 'Evento no encontrado' });
-	}
+    const event = await models.event.findById(id);
+    return res.json({ event });
+  } catch (_) {
+    return res.status(409).json({ error: "Evento no encontrado" });
+  }
 };
 
-const suprime =  async (req, res) => {
-          try {
-            const { id } = req.params;
-    
-            await models.event.findByIdAndRemove(id); //TODO: check it.
-            // console.log({ res });
-            // if (!res) {
-            // 	return res.status(409).json(false);
-            // }
-    
-            return res.json(true);
-        } catch (_) {
-            return res.status(409).json(false);
-        }
-    };
+const suprime = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    await models.event.findByIdAndRemove(id); //TODO: check it.
+    // console.log({ res });
+    // if (!res) {
+    // 	return res.status(409).json(false);
+    // }
+
+    return res.json(true);
+  } catch (_) {
+    return res.status(409).json(false);
+  }
+};
 
 module.exports = {
-create,
-all,
-get,
-suprime,
+  create,
+  all,
+  get,
+  suprime,
 };
-                     
