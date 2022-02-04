a8992fe7cc0f8e3199ba9b20261b2ce6223a7eee
[Arranzazu] <[Arranzazu@outlook.es]>
2022-02-04 06:32:32 +0100

    make it better

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/inventario")
+.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gilqp.mongodb.net/inventario?retryWrites=true&w=majority`)
+// .connect("mongodb+srv://Edu81:Edu81@cluster0.gilqp.mongodb.net/inventario?retryWrites=true&w=majority")
+// .connect("mongodb://localhost/inventario")
   .then(() => {
     console.log("Base de Datos Mongoose INV conectada");
   })
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config(); 
+
+
 const server = require("./server");
 require("./database");
 
